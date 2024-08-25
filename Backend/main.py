7db9fd1ce1b58f5bd55ac13a1b2fdf47a7b0a182
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2024-08-25 23:56:07 +0300

    Add files via upload

--- Backend/main.py
+++ Backend/main.py
@@ -12,9 +12,10 @@
 """
 
 from flask import Flask, request, jsonify, send_from_directory, url_for, redirect, session
-from dotenv import load_dotenv, dotenv_values
-from flask_socketio import SocketIO, emit
-from datetime import datetime, timezone, timedelta
+from dotenv import load_dotenv
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from datetime import datetime, timezone
 import threading
 import platform
 import argparse
@@ -22,63 +23,12 @@ import psutil
 import sys
 import os
 
-from flask_cors import CORS
-
 from Modules.logger import init_logger
 from Modules.commands import Commands
 from Modules.server import Server
 from Modules.utils import Handlers
 from Modules.controller import Controller
 
-
-class FileServer:
-    def __init__(self, main_path):
-        self.main_path = main_path
-        self.logger = Backend.logger
-
-    def get_files(self):
-        directory = request.args.get('directory')
-        if not directory:
-            return jsonify({'error': 'Directory parameter is missing'}), 400
-        
-        # Construct the full directory path
-        full_path = os.path.join(self.main_path, directory)
-
-        # Check if the directory exists
-        if not os.path.exists(full_path) or not os.path.isdir(full_path):
-            self.logger.debug(f"Directory not found: {full_path}")
-            try:
-                os.makedirs(full_path, exist_ok=True)
-            except Exception as e:
-                self.logger.error(f"Failed to create directory {full_path}: {e}")
-                return jsonify({'error': 'Failed to create directory.'}), 500
-
-        # Fetch the list of files
-        try:
-            file_list = os.listdir(full_path)
-
-            # Filter and construct URLs for the images
-            image_files = [
-                url_for('serve_static', filename=f"{directory}/{f}")
-                for f in file_list if f.endswith(('.jpg', '.png'))
-            ]
-            tasks_files = [f for f in file_list if f.startswith('tasks')]
-            sysinfo_files = [f for f in file_list if f.startswith('sysinfo')]
-
-            self.logger.info(f"Files retrieved from {full_path}: {file_list}")
-            return jsonify({
-                'numOfFiles': len(file_list),
-                'fileList': {
-                    'images': image_files,
-                    'tasks': tasks_files,
-                    'sysinfo': sysinfo_files
-                }
-            })
-        
-        except Exception as e:
-            self.logger.error(f"Error fetching files from {full_path}: {e}")
-            return jsonify({'error': 'Failed to fetch files.'}), 500
-        
 
 class Backend:
     def __init__(self, logger, main_path, log_path, server, version, server_ip, port):
@@ -95,8 +45,7 @@ class Backend:
         self.rows = []
         self.temp = []
         self.temp_rows = []
-        self.file_list = []
-        self.num_files = 0
+        self.files_info = ""
 
         self.app = Flask(__name__)
         CORS(self.app)
@@ -117,8 +66,6 @@ class Backend:
         self.sio.event('connect')(self.handle_connect)
 
         self.app.route('/images/<machine_name>/<path:filename>')(self.serve_images)
-        self.app.route('/tasks/<machine_name>/<path:filename>')(self.serve_tasks)
-        self.app.route('/sysinfo/<machine_name>/<path:filename>')(self.serve_sysinfo)
         self.app.route('/get_files', methods=['GET'])(self.get_files)
 
         self.app.errorhandler(404)(self.page_not_found)
@@ -135,9 +82,11 @@ class Backend:
         self.app.route('/wifi', methods=['POST'])(self.get_wifi)
 
     def on_event(self, data):
+        self.logger.debug(f"Event data: {data}")
         pass
 
     def handle_connect(self):
+        self.logger.debug(f"Connect event")
         pass
 
     def serve_images(self, machine_name, filename):
@@ -145,22 +94,12 @@ class Backend:
         images_dir = os.path.join(os.getenv('MAIN_PATH'), machine_name, 'images')
         return send_from_directory(images_dir, filename)
 
-    def serve_tasks(self, machine_name, filename):
-        # Construct the full path to the tasks directory
-        tasks_dir = os.path.join(self.main_path, machine_name, 'tasks')
-        return send_from_directory(tasks_dir, filename)
-
-    def serve_sysinfo(self, machine_name, filename):
-        # Construct the full path to the sysinfo directory
-        sysinfo_dir = os.path.join(self.main_path, machine_name, 'sysinfo')
-        return send_from_directory(sysinfo_dir, filename)
-
     def download_file(self, filename):
-        self.logger.debug(f"Serving file: {filename}...")
+        self.logger.info(f"Serving file: {filename}...")
         return send_from_directory('static', filename, as_attachment=True)
 
     def page_not_found(self, error) -> jsonify:
-        self.logger.info(fr'Error 404: Directory not found.')
+        self.logger.error(fr'Error 404: Directory not found.')
         return jsonify({'error': 'Directory not found'}), 404
 
     def find_matching_endpoint(self, data) -> str:
@@ -171,42 +110,33 @@ class Backend:
             return next(iter(matching_endpoints), None)
 
         except AttributeError as e:
-            self.logger.error(e)
+            self.logger.error(f"Error while matching endpoints: {e}")
             return data['checkedItems']
 
     def get_files(self):
         directory = request.args.get('directory')
+        self.logger.debug(f"Get Files: Directory: {directory}")
         if not directory:
             return jsonify({'error': 'Directory parameter is missing'}), 400
 
         images_dir = os.path.join(self.main_path, directory, 'images')
-        tasks_dir = os.path.join(self.main_path, directory, 'tasks')
-        sysinfo_dir = os.path.join(self.main_path, directory, 'sysinfo')
+        self.logger.debug(f"Get Files: Images Dir: {images_dir}")
 
         try:
             # Get file names
             image_files = os.listdir(images_dir) if os.path.exists(images_dir) else []
-            tasks_files = os.listdir(tasks_dir) if os.path.exists(tasks_dir) else []
-            sysinfo_files = os.listdir(sysinfo_dir) if os.path.exists(sysinfo_dir) else []
 
-            # Construct full URLs using url_for with the correct routes
+            self.logger.debug(f"Get Files: Compiling images urls...")
             image_urls = [
                 url_for('serve_images', machine_name=directory, filename=f, _external=True) for f in image_files
             ]
-            tasks_urls = [
-                url_for('serve_tasks', machine_name=directory, filename=f, _external=True) for f in tasks_files
-            ]
-            sysinfo_urls = [
-                url_for('serve_sysinfo', machine_name=directory, filename=f, _external=True) for f in sysinfo_files
-            ]
 
             return jsonify({
-                'images': image_urls,
-                'tasks': tasks_urls,
-                'sysinfo': sysinfo_urls
+                'images': image_urls
             })
 
         except Exception as e:
+            self.logger.error(f"Error while retreiving files: {e}")
             return jsonify({'error': str(e)}), 500
 
     def get_file_content(self):
@@ -216,8 +146,6 @@ class Backend:
             file_content = file.read()
 
         return jsonify({'fileContent': file_content})
-
-    import os
 
     def count_files(self):
         self.logger.info("Running count_files()...")
@@ -250,12 +178,12 @@ class Backend:
 
         return folder_details
 
-
     def control(self):
         self.logger.info(f"<Control>")
 
         restarted = []
         updated = []
+
         data = request.get_json()
         self.logger.debug(f"Command: {data}")
 
@@ -281,7 +209,7 @@ class Backend:
 
     def discover(self):
         matching_endpoint = self.find_matching_endpoint(data=None)
-        self.logger.debug(f"Calling self.commands.discover()...")
+        self.logger.debug(f"Calling self.commands.discover({matching_endpoint})...")
         netMap = self.commands.call_discover(matching_endpoint)
         files = self.count_files()
         self.logger.info(f"Netmap: {netMap}\nFiles: {files}\n")
@@ -289,7 +217,7 @@ class Backend:
 
     def get_ex_ip(self):
         matching_endpoint = self.find_matching_endpoint(data=None)
-        self.logger.info("Calling commands.get_ex_ip()...")
+        self.logger.info(f"Calling commands.get_ex_ip({matching_endpoint})...")
         ip = self.commands.ex_ip(matching_endpoint)
         return jsonify({'ip': ip})
 
@@ -298,14 +226,14 @@ class Backend:
         if not matching_endpoint:
             return jsonify({'wifi': 'No endpoint found.'})
 
-        self.logger.info("Calling commands.get_nearby_wifi()...")
+        self.logger.info(f"Calling commands.get_nearby_wifi({matching_endpoint})...")
         networks_data, files = self.commands.get_nearby_wifi(matching_endpoint)
         return jsonify({'wifi': networks_data, 'files': files})
 
     def clear_local(self):
         matching_endpoint = self.find_matching_endpoint(data=None)
-        path = self.handlers.clear_local(matching_endpoint)
-        if path:
+        isLocalCleared = self.handlers.clear_local(matching_endpoint)
+        if isLocalCleared:
             return jsonify({'message': f'Local dir cleared'})
 
         return jsonify({'error': f'Something went wrong'})
@@ -334,15 +262,14 @@ class Backend:
 
         self.selected_row_data = request.get_json()
         self.logger.debug(f"Selected Row Data: {self.selected_row_data}")
+        
         checked_value = self.selected_row_data.get('checked')
-        row_value = self.selected_row_data.get('row')
-        self.logger.debug(f"Checked Value: {checked_value}")
-        self.logger.debug(f"Row Value: {row_value}")
+        row_value = self.selected_row_data.get('message')
+        self.logger.debug(f"Checked Value: {checked_value} | Row Value: {row_value}")
         
         if row_value == 'clear_shell':
             self.station = False
-            self.file_list = []
-            self.num_files = 0
+            self.files_info = ""
 
             return jsonify({
                 "data": {
@@ -352,53 +279,63 @@ class Backend:
             })
 
         if 'conn' in self.selected_row_data:
+            self.logger.debug(f"Found 'conn'!")
+
             if self.server.endpoints:
                 time_now = datetime.now(timezone.utc)
+                self.logger.debug(f"Settting session login time...")
                 session['login_time'] = time_now
 
                 for endpoint in self.server.endpoints:
                     try:
                         self.logger.debug(f"Endpoint.client_mac: {endpoint.client_mac}")
                         if endpoint.client_mac == self.selected_row_data['client_mac']:
+                            self.logger.debug(f"Match found!")
+
                             dir_path = os.path.join(self.main_path, endpoint.ident)
                             self.logger.debug(f"Dir Path: {dir_path}")
 
                             if not os.path.exists(dir_path):
-                                os.makedirs(dir_path, exist_ok=True)
+                                self.logger.info(f"Calling 'self.handlers.handle_local_dir(endpoint)'...")
+                                self.handlers.handle_local_dir(endpoint)
 
-                            self.num_files = self.count_files()
-                            self.file_list = os.listdir(dir_path)
+                            self.files_info = self.count_files()
+                            self.subdirs = os.listdir(dir_path)
 
-                    except KeyError:
+                    except KeyError as k:
+                        self.logger.debug(f"Key error caught: {k}")
                         pass
 
                 try:
-                    self.logger.info(f'row: {self.selected_row_data}\n'
-                                    f'station: {self.station}\n'
-                                    f'num_files: {self.num_files}\n'
-                                    f'file_list: {self.file_list}'
-                                    )
+                    self.logger.info(
+                        f'row: {self.selected_row_data}\n'
+                        f'station: {self.station}\n'
+                        f'files_info: {self.files_info}\n'
+                        f'subdirs: {self.subdirs}'
+                    )
                     
-                except AttributeError:
+                except AttributeError as a:
+                    self.logger.debug(f"Attribute error caught: {a}")
                     pass
 
                 return jsonify({
                     "data": {
                         'row': self.selected_row_data,
                         'station': self.station,
-                        'num_files': self.num_files,
-                        'file_list': self.file_list,
-                        'checked': checked_value  # Send back the checkbox state as well
+                        'files_info': self.files_info,
+                        'subdirs': self.subdirs,
+                        'checked': checked_value
                     },
                     "Status": 200
                 })
 
             self.commands.shell_target = []
             self.station = False
-            self.logger.info(fr'No connected stations.')
+            self.logger.info(f"No connected stations.")
+
             return jsonify({
                 "data": {
-                    'message': 'No connected stations.'
+                    "message": "No connected stations."
                 },
                 "Status": 200
             })
@@ -406,15 +343,13 @@ class Backend:
         else:
             return jsonify({
                 "data": {
-                    'station': self.station,
-                    'checked': checked_value  # Send back the checkbox state as well
+                    "station": self.station
                 },
                 "Status": 200
             })
 
-
     def index(self):
-        self.logger.info(f'User {os.getenv("USER")} logged in successfully')
+        self.logger.info(f'INDEX: User {os.getenv("USER")} logged in successfully')
         matching_endpoint = None
         self.commands = Commands(self.main_path, self.log_path, matching_endpoint,
                                 self.server.remove_lost_connection)
@@ -442,23 +377,8 @@ class Backend:
 
         return jsonify(res)
 
-    
     def run(self):
         self.sio.run(self.app, host=self.server_ip, port=self.port)
-
-def check_platform(main_path):
-    if platform.system() == 'Windows':
-        main_path = main_path.replace('/', '\\')
-        log_path = os.path.join(main_path, os.getenv('LOG_FILE'))
-        return main_path, log_path
-
-    elif platform.system() == 'Linux':
-        log_path = os.path.join(main_path, os.getenv('LOG_FILE'))
-        return main_path, log_path
-
-    else:
-        print("Unsupported operating system.")
-        sys.exit(1)
 
 
 def main(**kwargs):
@@ -492,7 +412,22 @@ def main(**kwargs):
     server.listener()
 
 
-if __name__ == '__main__':
+def check_platform(main_path):
+    if platform.system() == 'Windows':
+        main_path = main_path.replace('/', '\\')
+        log_path = os.path.join(main_path, os.getenv('LOG_FILE'))
+        return main_path, log_path
+
+    elif platform.system() == 'Linux':
+        log_path = os.path.join(main_path, os.getenv('LOG_FILE'))
+        return main_path, log_path
+
+    else:
+        print("Unsupported operating system.")
+        sys.exit(1)
+
+
+def load_config_from_args_and_env():
     parser = argparse.ArgumentParser(description='HandsOff-Server')
     parser.add_argument('-wp', '--web_port', type=int, help='Web port')
     parser.add_argument('-sp', '--server_port', type=int, help='Server port')
@@ -501,12 +436,26 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     load_dotenv()
+
     web_port = args.web_port if args.web_port else int(os.getenv('WEB_PORT'))
     server_port = args.server_port if args.server_port else int(os.getenv('SERVER_PORT'))
     main_path = args.main_path if args.main_path else str(os.getenv('MAIN_PATH'))
-    main_path, log_path = check_platform(main_path)
     server_ip = args.server_ip if args.server_ip else str(os.getenv('SERVER_IP'))
     server_version = os.getenv('SERVER_VERSION')
+
+    local_download_path = os.path.join(main_path, 'LocalFiles')
+    if not os.path.isdir(local_download_path):
+        try:
+            os.makedirs(local_download_path)
+        except Exception as e:
+            print(f"Error creating '{local_download_path}': {e}")
+
+    return web_port, server_port, main_path, server_ip, server_version, local_download_path
+
+
+if __name__ == '__main__':
+    web_port, server_port, main_path, server_ip, server_version, local_download_path = load_config_from_args_and_env()
+    main_path, log_path = check_platform(main_path)
 
     kwargs = {
         'web_port': web_port,
